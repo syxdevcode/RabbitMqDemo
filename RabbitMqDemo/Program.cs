d4d353a11c1088b3be5cb27d6f2a41736c4c3c99
ShiyxProjectCode <syxtest01@gmail.com>
2018-07-05 14:47:56 +0800

    添加项目文件。

---
+++ RabbitMqDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
