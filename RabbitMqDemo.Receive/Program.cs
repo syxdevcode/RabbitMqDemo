533c33e27cf935c73fdc4503b4ffe34dddbbaf41
ShiyxProjectCode <syxtest01@gmail.com>
2018-07-13 17:32:53 +0800

    添加

--- RabbitMqDemo.Receive/Program.cs
+++ RabbitMqDemo.Receive/Program.cs
@@ -11,48 +11,244 @@ namespace RabbitMqDemo.Receive
     {
         static void Main(string[] args)
         {
-            //1.实例化连接工厂
+            Test5();
+            Console.ReadLine();
+        }
+
+        static void Test1()
+        {
+            // 实例化连接工厂
+            // 加证书
+            //ConnectionFactory factory = new ConnectionFactory()
+            //{
+            //    UserName = "admin",
+            //    Password = "admin",
+            //    Ssl = new SslOption()
+            //    {
+            //        CertPath = @"E:\git\RabbitMqDemo\RabbitMqDemo.Receive\server.pfx",
+            //        CertPassphrase = "123123",
+
+            //        AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+            //                                    SslPolicyErrors.RemoteCertificateChainErrors,
+            //        Enabled = true
+            //    },
+            //    //AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
+            //    RequestedHeartbeat = 60,
+            //    Port = 5673
+            //};
+
             ConnectionFactory factory = new ConnectionFactory()
             {
                 UserName = "admin",
                 Password = "admin",
                 AutomaticRecoveryEnabled = true,
-                Ssl = new SslOption()
-                {
-                    CertPath = @"E:\git\RabbitMqDemo\RabbitMqDemo.Receive\server.pfx",
-                    CertPassphrase = "123123",
-                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
-                                                SslPolicyErrors.RemoteCertificateChainErrors,
-                    Enabled = true
-                },
-                //AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
-                RequestedHeartbeat = 60,
-                Port = 5673,
+                Port = 5672,
                 TopologyRecoveryEnabled = true
             };
 
-            //2. 建立连接
+            //  建立连接
             using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
             {
-                //3. 创建信道
+                //  创建信道
                 using (var channel = connection.CreateModel())
                 {
-                    //4. 申明队列
-                    channel.QueueDeclare(queue: "test1", durable: true, exclusive: false, autoDelete: false, arguments: null);
-                    //5. 构造消费者实例
+                    //申明direct类型exchange
+                    channel.ExchangeDeclare(exchange: "directEC", type: "direct");
+
+                    //绑定队列到direct类型exchange，需指定路由键routingKey
+                    channel.QueueBind(queue: "green-queue", exchange: "directEC", routingKey: "green");
+
+                    // 申明队列
+                    channel.QueueDeclare(queue: "green-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    // 设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                    // 构造消费者实例
                     var consumer = new EventingBasicConsumer(channel);
-                    //6. 绑定消息接收后的事件委托
+                    // 绑定消息接收后的事件委托
                     consumer.Received += (model, ea) =>
                     {
                         var message = Encoding.UTF8.GetString(ea.Body);
                         Console.WriteLine(" [x] Received {0}", message);
-                        Thread.Sleep(500);//模拟耗时
-                        Console.WriteLine(" [x] Done");
+                        // TODO 注：耗时过长（测试使用5000）,，使用证书模式手动确认会报错，原因不明
+                        Thread.Sleep(2000);//模拟耗时 
+                        Console.WriteLine(" [x] Done {0}", message);
+
+                        // 发送消息确认信号（手动消息确认）
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                     };
-                    //7. 启动消费者
-                    channel.BasicConsume(queue: "test1", autoAck: true, consumer: consumer);
+                    //  启动消费者
+                    /*
+                     autoAck:true；自动进行消息确认
+                     autoAck:false；关闭自动消息确认，通过调用BasicAck方法手动进行消息确认
+                     */
+                    //channel.BasicConsume(queue: "test1", autoAck: true, consumer: consumer);
+                    channel.BasicConsume(queue: "green-queue", autoAck: false, consumer: consumer);
                     Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void Test2()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Port = 5672,
+                TopologyRecoveryEnabled = true
+            };
+
+            //  建立连接
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                //  创建信道
+                using (var channel = connection.CreateModel())
+                {
+                    //申明direct类型exchange
+                    channel.ExchangeDeclare(exchange: "directEC", type: "direct");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queueName, exchange: "directEC", routingKey: "direct-key");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    //  绑定消息接收后的事件委托
+                    consumer.Received += (model, ea) =>
+                    {
+                        // var body = ea.Body;涉及到闭包，必须赋予变量
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                        Console.WriteLine(" [x] Done1 {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: true,
+                                         consumer: consumer);
+                    //channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    Console.WriteLine(" Press [enter] to exit.");
+
+                    Console.ReadLine(); // 必须存在
+                }
+            }
+        }
+
+        static void Test3()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Port = 5672,
+                TopologyRecoveryEnabled = true
+            };
+
+            //  建立连接
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                //  创建信道
+                using (var channel = connection.CreateModel())
+                {
+                    //申明direct类型exchange
+                    channel.ExchangeDeclare(exchange: "fanoutEC", type: "fanout");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    // 绑定队列到指定fanout类型exchange，无需指定路由键
+                    channel.QueueBind(queue: queueName, exchange: "fanoutEC", routingKey: "");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    //  绑定消息接收后的事件委托
+                    consumer.Received += (model, ea) =>
+                    {
+                        // var body = ea.Body;涉及到闭包，必须赋予变量
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                        Console.WriteLine(" [x] Done1 {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: true,
+                                         consumer: consumer);
+                    //channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    Console.WriteLine(" Press [enter] to exit.");
+
+                    Console.ReadLine(); // 必须存在
+                }
+            }
+        }
+
+        static void Test4()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Port = 5672,
+                TopologyRecoveryEnabled = true
+            };
+
+            //  建立连接
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                //  创建信道
+                using (var channel = connection.CreateModel())
+                {
+                    //申明direct类型exchange
+                    channel.ExchangeDeclare(exchange: "topicEC", type: "topic");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    //绑定队列到topic类型exchange，需指定路由键routingKey
+                    channel.QueueBind(queue: queueName, exchange: "topicEC", routingKey: "#.*.fast");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    //  绑定消息接收后的事件委托
+                    consumer.Received += (model, ea) =>
+                    {
+                        // var body = ea.Body;涉及到闭包，必须赋予变量
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                        Console.WriteLine(" [x] Done1 {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: true,
+                                         consumer: consumer);
+                    //channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    Console.WriteLine(" Press [enter] to exit.");
+
+                    Console.ReadLine(); // 必须存在
+                }
+            }
+        }
+
+        static void Test5()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Port = 5672,
+                TopologyRecoveryEnabled = true
+            };
+
+            //  建立连接
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                //  创建信道
+                using (var channel = connection.CreateModel())
+                {
+
                 }
             }
         }
--- RabbitMqDemo.Send/Program.cs
+++ RabbitMqDemo.Send/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Net;
 using System.Net.Security;
@@ -11,7 +12,16 @@ namespace RabbitMqDemo.Send
     {
         static void Main(string[] args)
         {
-            //ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            Test5();
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 链接，工作队列，发送/接收消息，轮训分发，消息确认，消息持久化，公平分发
+        /// </summary>
+        static void Test1()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
             ConnectionFactory factory = new ConnectionFactory()
             {
                 UserName = "admin",
@@ -31,26 +41,201 @@ namespace RabbitMqDemo.Send
                 TopologyRecoveryEnabled = true
             };
 
-            //第一步：创建connection 
+            // 创建connection 
             using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
             {
-                //第二步：创建一个channel信道
+                // 创建一个channel信道
                 using (var channel = connection.CreateModel())
                 {
-                    //第三步：申明队列 durable:是否持久化
+                    //将消息标记为持久性 - 将IBasicProperties.SetPersistent设置为true
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    // 申明队列 durable:是否持久化
                     var result = channel.QueueDeclare(queue: "test1", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-                    for (int i = 0; i < int.MaxValue; i++)
+                    for (int i = 0; i < 21; i++)
                     {
                         //构建byte消息数据包
                         var body = Encoding.UTF8.GetBytes(i.ToString() + "test");
                         channel.BasicPublish(exchange: "", routingKey: "test1", basicProperties: null, body: body);
                         Console.WriteLine("{0} 推送成功", i);
-                        Thread.Sleep(500);
+                        Thread.Sleep(2000);
                     }
                 }
             }
-            Console.Read();
+        }
+
+        /// <summary>
+        /// direct
+        /// </summary>
+        static void Test2()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Ssl = new SslOption()
+                {
+                    CertPath = @"E:\git\RabbitMqDemo\RabbitMqDemo.Send\server.pfx",
+                    CertPassphrase = "123123",
+                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+                                                SslPolicyErrors.RemoteCertificateChainErrors,
+                    Enabled = true
+                },
+                //AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
+                RequestedHeartbeat = 60,
+                Port = 5673,
+                TopologyRecoveryEnabled = true
+            };
+
+            // 创建connection 
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                // 创建一个channel信道
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "directEC", type: "direct");
+
+                    //将消息标记为持久性 - 将IBasicProperties.SetPersistent设置为true
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    for (int i = 0; i < 21; i++)
+                    {
+                        //构建byte消息数据包
+                        var body = Encoding.UTF8.GetBytes(i.ToString() + "test");
+                        channel.BasicPublish(exchange: "directEC", routingKey: "direct-key", basicProperties: null, body: body);
+                        Console.WriteLine("{0} 推送成功", i);
+                        //Thread.Sleep(2000);
+                    }
+                }
+            }
+        }
+
+        static void Test3()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Ssl = new SslOption()
+                {
+                    CertPath = @"E:\git\RabbitMqDemo\RabbitMqDemo.Send\server.pfx",
+                    CertPassphrase = "123123",
+                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+                                                SslPolicyErrors.RemoteCertificateChainErrors,
+                    Enabled = true
+                },
+                //AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
+                RequestedHeartbeat = 60,
+                Port = 5673,
+                TopologyRecoveryEnabled = true
+            };
+
+            // 创建connection 
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                // 创建一个channel信道
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "fanoutEC", type: "fanout");
+
+                    //将消息标记为持久性 - 将IBasicProperties.SetPersistent设置为true
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    for (int i = 0; i < 21; i++)
+                    {
+                        //构建byte消息数据包
+                        var body = Encoding.UTF8.GetBytes(i.ToString() + "test");
+
+                        //发布到指定exchange，fanout类型无需指定routingKey
+                        channel.BasicPublish(exchange: "fanoutEC", routingKey: "", basicProperties: properties, body: body);
+                        Console.WriteLine("{0} 推送成功", i);
+                        //Thread.Sleep(2000);
+                    }
+                }
+            }
+        }
+
+        static void Test4()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Ssl = new SslOption()
+                {
+                    CertPath = @"E:\git\RabbitMqDemo\RabbitMqDemo.Send\server.pfx",
+                    CertPassphrase = "123123",
+                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+                                                SslPolicyErrors.RemoteCertificateChainErrors,
+                    Enabled = true
+                },
+                //AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
+                RequestedHeartbeat = 60,
+                Port = 5673,
+                TopologyRecoveryEnabled = true
+            };
+
+            // 创建connection 
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                // 创建一个channel信道
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "topicEC", type: "topic");
+
+                    //将消息标记为持久性 - 将IBasicProperties.SetPersistent设置为true
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    for (int i = 0; i < 21; i++)
+                    {
+                        //构建byte消息数据包
+                        var body = Encoding.UTF8.GetBytes(i.ToString() + "test");
+
+                        ///发布到topic类型exchange，必须指定routingKey
+                        channel.BasicPublish(exchange: "topicEC", routingKey: "first.green.fast", basicProperties: properties, body: body);
+                        Console.WriteLine("{0} 推送成功", i);
+                        //Thread.Sleep(2000);
+                    }
+                }
+            }
+        }
+
+        static void Test5()
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                Ssl = new SslOption()
+                {
+                    CertPath = @"E:\git\RabbitMqDemo\RabbitMqDemo.Send\server.pfx",
+                    CertPassphrase = "123123",
+                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+                                                SslPolicyErrors.RemoteCertificateChainErrors,
+                    Enabled = true
+                },
+                //AuthMechanisms = new AuthMechanismFactory[] { new ExternalMechanismFactory() },
+                RequestedHeartbeat = 60,
+                Port = 5673,
+                TopologyRecoveryEnabled = true
+            };
+
+            // 创建connection 
+            using (var connection = factory.CreateConnection(new string[1] { "192.168.0.115" }))
+            {
+                 
+            }
         }
     }
 }
+
+
